205b44b6ef3a81fbb96a75419a976f3e128cace7
Alberto <delvillano.alberto@gmail.com>
2023-02-15 17:25:50 +0100

    New attempt at better Trait

--- src/main/java/insane96mcp/tinkersexpansion/modifiers/D20Modifier.java
+++ src/main/java/insane96mcp/tinkersexpansion/modifiers/D20Modifier.java
@@ -2,27 +2,34 @@ package insane96mcp.tinkersexpansion.modifiers;
 
 import insane96mcp.tinkersexpansion.TinkersExpansion;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.Level;
 import slimeknights.tconstruct.TConstruct;
 import slimeknights.tconstruct.library.modifiers.Modifier;
-import slimeknights.tconstruct.library.modifiers.util.ModifierLevelDisplay;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
+import slimeknights.tconstruct.tools.TinkerModifiers;
 
 import java.util.List;
 
 @Mod.EventBusSubscriber(modid = TinkersExpansion.MOD_ID)
 public class D20Modifier extends Modifier {
 
-    private static final int[] DIE_ROLLS = {0, 20, 12, 10, 8, 6, 4};
+    //private static final int[] DIE_ROLLS = {0, 20, 12, 10, 8, 6, 4};
 
-    private static final ModifierLevelDisplay NAME = new ModifierLevelDisplay.UniqueForLevels(6);
+    //private static final ModifierLevelDisplay NAME = new ModifierLevelDisplay.UniqueForLevels(6);
 
     @Override
     public Component getDisplayName(int level) {
-        return NAME.nameForLevel(this, level);
+        //return NAME.nameForLevel(this, level);
+        Component component = this.applyStyle(new TranslatableComponent(this.getTranslationKey()));
+        if (level > 1)
+            return component.copy().append(this.applyStyle(new TextComponent("+%d".formatted(level - 1))));
+        else
+            return component;
     }
 
     @Override
@@ -32,16 +39,18 @@ public class D20Modifier extends Modifier {
 
     @Override
     public List<ItemStack> processLoot(IToolStackView tool, int level, List<ItemStack> generatedLoot, LootContext context) {
-        if (level > DIE_ROLLS.length - 1)
-            level = DIE_ROLLS.length - 1;
-        int die = DIE_ROLLS[level];
-        int roll = RANDOM.nextInt(die);
-        if (roll != 0 && roll != DIE_ROLLS[level] - 1)
+        if (tool.getModifierLevel(TinkerModifiers.silky.get()) > 0)
             return generatedLoot;
-        int luck = getAmountWithDecimalChance(context.getLuck());
+        /*if (level > DIE_ROLLS.length - 1)
+            level = DIE_ROLLS.length - 1;
+        int luck = getAmountWithDecimalChance(context.getLuck());*/
+        int die = 20 + (level - 1);
+        int roll = RANDOM.nextInt(die) + 1;
+        if (roll > 1 && roll < 19)
+            return generatedLoot;
 
         ItemStack toModify = generatedLoot.get(RANDOM.nextInt(generatedLoot.size()));
-        if (roll == 0) {
+        if (roll == 1) {
             TConstruct.LOG.log(Level.INFO, "fail");
             toModify.shrink(1);
         }
